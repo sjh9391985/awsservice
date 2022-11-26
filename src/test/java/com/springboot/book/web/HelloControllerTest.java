989d80ec6a6de59e1cedb8c4772b07626ea3b2f4
sjh9391985 <sjh9391985@gmail.com>
2022-11-27 03:12:28 +0900

    DTO 단위테스트 코드작성

--- src/test/java/com/springboot/book/web/HelloControllerTest.java
+++ src/test/java/com/springboot/book/web/HelloControllerTest.java
@@ -2,7 +2,9 @@ package com.springboot.book.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -26,6 +29,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
             .andExpect(status().isOk())
             .andExpect(content().string(hello));
+
+    }
+
+    @Test
+    public void helloDto_return() throws Exception {
+
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is(name)))
+            .andExpect(jsonPath("$.amount", is(amount)));
 
     }
 
