142a8fa2bea3d79e6d7300d026ac07cf8bebc9ec
sjh9391985 <sjh9391985@gmail.com>
2022-12-05 21:36:10 +0900

    Spring security OAuth 설정코드 주석

--- src/main/java/com/springboot/book/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot/book/config/auth/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity // spring security 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -15,19 +15,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
-            .headers().frameOptions().disable()
+            .headers().frameOptions().disable() // h2-console 화면을 사용하기위해 옵션 disable
             .and()
-            .authorizeRequests()
+            .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점, authorizeRequests가 선언되야 antMatchers 옵션사용가능
             .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
             .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-            .anyRequest().authenticated()
+            .anyRequest().authenticated() // 설정된 값들 이외 나머지 URL들을 나타냄
             .and()
             .logout()
-            .logoutSuccessUrl("/")
+            .logoutSuccessUrl("/") // logout 성공시 '/' 주소로 이동
             .and()
-            .oauth2Login()
-            .userInfoEndpoint()
-            .userService(customOAuth2UserService);
+            .oauth2Login() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
+            .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정들을 담당함
+            .userService(customOAuth2UserService); // 소셜 로그인 성공 시 후속 조치를 진행할 인터페이스의 구현체를 등록
     }
 
 }
